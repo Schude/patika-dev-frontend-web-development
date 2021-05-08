72f9af16c4dcbeaea2b64ec4aa95472f83cfc040
Schude <nemli.serkan2@gmail.com>
2021-05-08 22:40:43 +0300

    Local Storage funcs

--- JavaScript/Odev-2/js/index.js
+++ JavaScript/Odev-2/js/index.js
@@ -1,12 +1,25 @@
 let TODOS = [];
 
-const renderTodos = (todo) => {
-    let list = document.querySelector("#list");
+const renderTodos = () => {
+    resetList();
+    TODOS.map((todo) => {
+        let list = document.querySelector("#list");
 
-    let liElement = document.createElement("li");
-    liElement.appendChild(document.createTextNode(todo.text));
-    list.appendChild(liElement);
-    liElement.id = todo.id;
+        let liElement = document.createElement("li");
+        liElement.appendChild(document.createTextNode(todo.text));
+        if (todo.checked) {
+            liElement.classList.toggle("checked");
+        }
+        list.appendChild(liElement);
+        liElement.id = todo.id;
+    });
+};
+const resetList = () => {
+    let list = document.querySelector("#list");
+    let liDOM = document.querySelectorAll("li");
+    liDOM.forEach((element) => {
+        list.removeChild(element);
+    });
 };
 
 const handleSubmit = () => {
@@ -21,27 +34,41 @@ const handleChecked = () => {
         event.target.classList.toggle("checked");
         TODOS.map((todo) => {
             if (todo.id == event.target.id) {
-                // ==  kullanıyoruz çünkü sadece value karşılaştırması. (event.target.id string)
+                // ==  kullanıyoruz çünkü sadece value karşılaştırması. (event.target.id - string)
                 todo.checked = !todo.checked;
             }
         });
+        setLocalStoreData(); //Checked olduğunda LocalStore update ediliyor
     });
 };
 
-const getLocalStoreData = () => {};
+const getLocalStoreData = () => JSON.parse(localStorage.getItem("Todos"));
+
+getLocalStoreData();
 const setLocalStoreData = () => {
     localStorage.setItem("Todos", JSON.stringify(TODOS));
 };
 
+//Eğer kullanıcı ilk defa giriyorsa localStore'da bir boş array oluşturuyoruz.
+const createLocalStore = () => {
+    if (getLocalStoreData() === null) {
+        localStorage.setItem("Todos", JSON.stringify([]));
+    }
+};
 const addTodo = (text) => {
     let newTodo = {
-        id: TODOS.length + 1, //increment
+        id: Date.now(), //unique
         text,
         checked: false,
     };
 
     TODOS = [...TODOS, newTodo];
+    setLocalStoreData();
     renderTodos(newTodo);
 };
 const removeTodo = () => {};
+
+createLocalStore();
+TODOS = getLocalStoreData();
+renderTodos();
 handleChecked();
